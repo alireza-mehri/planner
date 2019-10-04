c7325bbc7878d930e32e84958e5cdf8a62374ea0
Alireza Mehri <mehri107@gmail.com>
2019-10-04 16:27:48 +0330

    add Home page base to design home UI

--- planner/lib/main.dart
+++ planner/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'morabi',
       theme: new ThemeData(
 
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.redAccent,
       ),
       home: new LoginPage(),
     );
