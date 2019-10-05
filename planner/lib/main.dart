b4f5d3bbec2e95409984b65d38cb3de823c4b025
Alireza <mehri107@gmail>
2019-10-05 19:29:52 +0330

    Add splash screen

--- planner/lib/main.dart
+++ planner/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:planner/ui/Home_page.dart';
 import 'package:planner/ui/login_page.dart';
+import 'package:planner/ui/splash_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -12,8 +13,15 @@ class MyApp extends StatelessWidget {
       title: 'morabi',
       theme: new ThemeData(
         primarySwatch: Colors.red,
+
       ),
-      home: new LoginPage(),
+        initialRoute: "/splash_screen",
+        routes: {
+          "/" : (context) => new Directionality(textDirection: TextDirection.ltr, child: LoginPage()),
+          "/splash_screen" : (context) => new Directionality(textDirection: TextDirection.rtl, child: new SplashScreen()),
+         // "/setting" : (context) => new SettingScreen(),
+          //"/new_chat" : (context) => new CreateChatScreen()
+        },
     );
   }
 }
